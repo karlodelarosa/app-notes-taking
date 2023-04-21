2cc561663f32f7d907cb2b5a7c35b9eec830e68a
karlodlrosa <jrosa@salespond.com>
2023-04-21 19:57:31 +0800

    feat-1-remodeled base layout and components

---
+++ src/components/atom/link/LinkBasic.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  text?: string,
+  page?: string
+}>()
+</script>
+
+<template>
+  <RouterLink :to="{ name: page }" class="text-black hover:text-gray-600 transition-all duration-100 ease-in">
+    {{ text }}
+  </RouterLink>
+</template>
---
+++ src/components/organism/SidebarPanel.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SidebarMenu from '../molecules/menu/SidebarMenu.vue'
+</script>
+
+<template>
+  <aside class="sidebar-panel">
+    <div class="sidebar-app-name">
+      <h1 class="">NOTE TAKING</h1>
+    </div>
+    <SidebarMenu />
+  </aside>
+</template>
+
+<style scoped>
+.sidebar-panel {
+  @apply flex-none w-full md:max-w-xs bg-accent-1 text-white;
+}
+
+.sidebar-app-name {
+  @apply h-[75px] bg-accent-3 flex flex-col items-center justify-center mb-[50px];
+}
+
+.sidebar-app-name > h1 {
+  @apply text-white text-xl font-bold;
+}
+</style>
